983dc2687b6b45f853de3614b4cfa07456336e43
fddeath <254075058+fddeath@users.noreply.github.com>
2026-01-13 18:28:02 +1000

    add: css[vars]

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
         :root {
             --font-size: 26px;
+            --background: rgb(32, 32, 32);
+            --color: rgb(255, 255, 255);
+            --color_2: rgb(30, 162, 195);
+            --color_3: rgba(0, 255, 123, 0.4);
+            --shadow: rgba(30, 162, 195, 0.5);
         }
         * {
             margin: 0;
@@ -21,6 +26,8 @@
             font-family: "Montserrat", sans-serif;
             font-optical-sizing: auto;
             font-style: normal;
+            background: var(--background);
+            color: var(--color);
         }
         .cont {
             position: absolute;
@@ -30,7 +37,7 @@
             width: fit-content;
         }
         h2 {
-            font-weight: 400;
+            font-weight: 600;
             display: flex;
             align-items: center;
             gap: 10px;
@@ -38,13 +45,25 @@
             &::before {
                 content: "\2601";
                 font-size: 50px;
-                filter: drop-shadow(0 0 10px #44cad4);
+                filter: drop-shadow(0 0 10px var(--shadow));
             }
+        }
+        h3 {
+            font-weight: 400;
         }
         form {
             display: flex;
             flex-direction: column;
             gap: 1rem;
+        }
+        .cat_cont {
+            display: flex;
+            flex-direction: column;
+            gap: 0.2rem;
+        }
+        .cat_input_cont {
+            display: flex;
+            gap: 0.5rem;
         }
         .input_cont {
             display: flex;
@@ -52,23 +71,38 @@
             height: 1rem;
         }
         input {
+            color: var(--color);
+            background: none;
             width: 100%;
             height: 100%;
-            padding: 2px 4px;
+            padding: 4px;
             outline: none;
             border: none;
-            border-bottom: 1px solid #000;
+            border-bottom: 1px solid var(--color);
             transition: 0.2s;
 
             &:focus {
-                border-color: #44cad4;
+                border-color: var(--color_2);
             }
+
             &:focus + label,
             &:not(:placeholder-shown) + label {
-                color: #44cad4;
-                background: #fff;
+                color: var(--color_2);
+                background: var(--background);
                 padding-inline: 2px;
-                bottom: -0.45rem;
+                bottom: -60%;
+                font-size: calc(var(--font-size) / 2);
+            }
+
+            &:not(:placeholder-shown, :focus) {
+                border-color: var(--color_3);
+            }
+
+            &:not(:placeholder-shown, :focus) + label {
+                color: var(--color_3);
+                background: var(--background);
+                padding-inline: 2px;
+                bottom: -60%;
                 font-size: calc(var(--font-size) / 2);
             }
         }
@@ -76,25 +110,28 @@
             color: var(--color);
             position: absolute;
             left: 0.4rem;
-            bottom: 0;
+            bottom: -4px;
             z-index: 1;
             transition: 0.2s;
             font-size: calc(var(--font-size) / 1.4);
+            font-weight: 300;
 
             &:hover {
                 cursor: text;
             }
         }
         #btn {
-            width: 100%;
-            height: 100%;
-            background: #fff;
-            border: 1px solid #000;
+            color: var(--color);
+            width: 10rem;
+            height: 1.5rem;
+            background: none;
+            border: 1px solid var(--color);
+            margin-top: 1rem;
 
             &:hover {
                 cursor: pointer;
-                color: #44cad4;
-                border-color: #44cad4;
+                color: var(--color_2);
+                border-color: var(--color_2);
                 transition: 0.1s;
             }
         }
@@ -104,45 +141,53 @@
     <div class="cont">
         <h2>Cloud space</h2>
         <form action="/index.php" method="POST">
-            <div class="input_cont">
-                <input
-                    type="text"
-                    name="site_name"
-                    id="site_name"
-                    placeholder=" "
-                >
-                <label for="site_name">Site name</label>
+            <div class="cat_cont">
+                <h3>Site settings</h3>
+                <div class="cat_input_cont">
+                    <div class="input_cont">
+                        <input
+                            type="text"
+                            name="site_name"
+                            id="site_name"
+                            placeholder=" "
+                        >
+                        <label for="site_name">Site name</label>
+                    </div>
+                </div>
             </div>
-            <div class="input_cont">
-                <input
-                    type="text"
-                    name="db_login"
-                    id="db_login"
-                    placeholder=" "
-                >
-                <label for="db_login">DB Login</label>
+            <div class="cat_cont">
+                <h3>DataBase</h3>
+                <div class="cat_input_cont">
+                    <div class="input_cont">
+                        <input
+                            type="text"
+                            name="db_login"
+                            id="db_login"
+                            placeholder=" "
+                        >
+                        <label for="db_login">DB Login</label>
+                    </div>
+                    <div class="input_cont">
+                        <input
+                            type="text"
+                            name="db_password"
+                            id="db_password"
+                            placeholder=" "
+                        >
+                        <label for="db_password">DB Password</label>
+                    </div>
+                    <div class="input_cont">
+                        <input
+                            type="text"
+                            name="db_name"
+                            id="db_name"
+                            placeholder=" "
+                        >
+                        <label for="db_name">DB Name</label>
+                    </div>
+                </div>
             </div>
-            <div class="input_cont">
-                <input
-                    type="text"
-                    name="db_password"
-                    id="db_password"
-                    placeholder=" "
-                >
-                <label for="db_password">DB Password</label>
-            </div>
-            <div class="input_cont">
-                <input
-                    type="text"
-                    name="db_name"
-                    id="db_name"
-                    placeholder=" "
-                >
-                <label for="db_name">DB Name</label>
-            </div>
-            <div class="input_btn">
-                <input id="btn" type="submit" value="Create">
-            </div>
+            <input id="btn" type="submit" value="Create">
         </form>
     </div>
 </body>
